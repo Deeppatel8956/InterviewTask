48b1605dbb31ae022cbeba2e2c877d583e3096db
Deeppatel8956 <110016757+Deeppatel8956@users.noreply.github.com>
2024-10-25 15:13:22 +0530

    Number php

---
+++ PHP/Number.php
@@ -0,0 +1,36 @@
+<?php
+// Online PHP compiler to run PHP program online
+// Print "Try programiz.pro" message
+// echo str("Try programiz.pro");
+$y = "dog";
+echo "the dooler $y";
+echo strtoupper("dog");
+echo strtolower("D");
+$x = "Hello World!";
+echo str_replace("World", "Dolly", $x);
+echo strrev("hello");
+echo trim(" gof ");
+$x = 5;
+$y = 10;
+$z = "$x . $y";
+echo "the answer $z";
+
+// simple concat
+$c = "hello";
+$d = "hello";
+
+echo " $c $d";
+
+// dot
+$x = "Hello";
+$y = "World";
+$z = $x . $y;
+echo $z;
+
+
+// slice method
+$x = "Hello World!";
+echo substr($x, -5, 3);
+
+
+?>
