1e4b88d0c90a315d6abe3510279da8a03b153558
Deeppatel8956 <110016757+Deeppatel8956@users.noreply.github.com>
2024-10-27 14:52:40 +0530

    Php math method

---
+++ PHP/Math.php
@@ -0,0 +1,16 @@
+<?php
+// Online PHP compiler to run PHP program online
+// Print "Try programiz.pro" message
+echo "Try programiz.pro";
+echo (max(100,200,300));
+echo (min(1000,200,300,400));
+echo (abs(-7.8));
+echo (sqrt(64));
+echo(round(0.60));
+echo(rand(10, 100));
+
+
+$d = 10;
+$f = 20;
+echo $d + $f
+?>
